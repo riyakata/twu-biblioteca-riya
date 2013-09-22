a463a52d4305c1b66ba27ea407678cba0e6b1c65
Riya Kataria <riyakata@thoughtworks.com>
2013-09-22 19:05:49 +0530

    Added books to library in its constructor

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -8,6 +8,17 @@ public class Library {
   public Library(Long id, String name) {
     this.id = id;
     this.name = name;
+
+    new Book(1L, "HarryPotter", "J.K. Rowling", false);
+    new Book(2L, "A Journey", "Tony Blair", false);
+    new Book(3L, "A week with Gandhi", "L. Fischer", false);
+    new Book(4L, "Anna Karenina", "Leo Tolstoy", false);
+    new Book(5L, "Das Capital", "Karl Marks", false);
+    new Book(6L, "Bandit Queen", "Mala Sen", false);
+    new Book(7L, "Guide", "R. K. Narayanan", false);
+    new Book(8L, "Hind Swaraj", "M. K. Gandhi", false);
+    new Book(9L, "Historica", "Herodotus", false);
+    new Book(10L, "I Van Ho", "Walter Scot", false);
   }
 
   public Long getId() {
